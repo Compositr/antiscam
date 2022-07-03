7b3967b6c440acad64637cb3513586f6171e40fc
Jim <compositr@compositr.dev>
2022-07-03 21:41:22 +1000

    feat: api
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ pages/api/v1/servers/query.ts
@@ -0,0 +1,78 @@
+import { prisma } from "../../../../lib/db";
+import { NextApiRequest, NextApiResponse } from "next";
+import StandardResponse from "../../../../typings/api/StandardResponse";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<StandardResponse>
+) {
+  if (req.method !== "GET")
+    return res.status(405).json({
+      message: "Method not allowed. Use GET",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.cursor))
+    return res.status(400).json({
+      message: "Cursor must be a string",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.id))
+    return res.status(400).json({
+      message: "ID must be a string",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.name))
+    return res.status(400).json({
+      message: "Name must be a string",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.size))
+    return res.status(400).json({
+      message: "Size must be a string",
+      data: null,
+    });
+
+  if (
+    !["TINY", "SMALL", "MEDIUM", "LARGE", "OPERATION", undefined].includes(
+      req.query.size
+    )
+  )
+    return res.status(400).json({
+      message: "Size must be one of TINY, SMALL, MEDIUM, LARGE, OPERATION",
+      data: null,
+    });
+
+  if (Array.isArray(req.query.serverId))
+    return res.status(400).json({
+      message: "ServerId must be a string",
+      data: null,
+    });
+
+  await prisma.$connect();
+  const query = await prisma.scamServer.findMany({
+    take: 10,
+    skip: req.query.cursor ? 1 : 0,
+    cursor: {
+      id: req.query.cursor || undefined,
+    },
+
+    where: {
+      id: req.query.id || undefined,
+      name: req.query.name || undefined,
+      size: (req.query.size as any) || undefined,
+      serverId: req.query.serverId || undefined,
+    },
+  });
+
+  return res.status(200).json({
+    message: "Success",
+    data: {
+      query,
+      cursor: query[9].id,
+    },
+  });
+}
