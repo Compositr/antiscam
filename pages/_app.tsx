b10df4a8380cbd4ad2f43a7213818e6b75027400
Jim <compositr@compositr.dev>
2022-07-03 20:47:32 +1000

    feat: navigation and base
    Signed-off-by: Jim <compositr@compositr.dev>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import Nav from "../components/common/Nav";
 
 const queryClient = new QueryClient();
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Nav />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
