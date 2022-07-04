f54edc2cc18682f630da24c515fc65051a94fbf7
Jim <compositr@compositr.dev>
2022-07-04 14:05:16 +1000

    feat: new nav
    Signed-off-by: Jim <compositr@compositr.dev>

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -6,82 +6,56 @@ export default function Nav() {
   const [hidden, setHidden] = useState(true);
 
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-      <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <Link href={"/"}>
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white hover:cursor-pointer">
-            Scam DB
-          </span>
-        </Link>
-        <button
-          onClick={() => setHidden(!hidden)}
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <svg
-            className="hidden w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div
-          className={`${hidden ? "hidden" : ""} w-full md:block md:w-auto`}
-          id="mobile-menu"
-        >
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <NavItem href="/" name="Home" />
-            <NavItem href="/invite/check" name="Invite Lookup" />
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/invite/check"}>Check Invite</Link>
+            </li>
           </ul>
         </div>
-      </div>
-    </nav>
-  );
-}
-
-export function NavItem({ href, name }: { href: string; name: string }) {
-  const router = useRouter();
-  if (href === router.pathname)
-    return (
-      <li>
-        <Link
-          href={href}
-          className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-          aria-current="page"
-        >
-          {name}
+        <Link passHref href={"/"}>
+          <span className="btn btn-ghost normal-case text-xl">Scam DB</span>
         </Link>
-      </li>
-    );
-  return (
-    <li>
-      <Link
-        href={href}
-        className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-      >
-        {name}
-      </Link>
-    </li>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-1">
+          <li className="mx-1">
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li className="mx-1">
+            <Link href={"/invite/check"}>Check Invite</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <Link href={"/invite/check"} passHref>
+          <button className="btn btn-primary">Check Invite</button>
+        </Link>
+      </div>
+    </div>
   );
 }
