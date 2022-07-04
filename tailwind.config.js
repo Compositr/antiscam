6dc87442fb49f72282ad80af17cdf7020cda68fe
Jim <compositr@compositr.dev>
2022-07-04 15:58:28 +1000

    feat: toggle theme
    Signed-off-by: Jim <compositr@compositr.dev>

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,4 +8,14 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["night", "light"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "night",
+  },
 };
