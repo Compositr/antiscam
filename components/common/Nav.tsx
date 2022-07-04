6a894b5ce7a828f5f466e0c0f497b6e2da3084b0
Jim <compositr@compositr.dev>
2022-07-04 12:06:55 +1000

    feat: nav link
    Signed-off-by: Jim <compositr@compositr.dev>

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -52,6 +52,7 @@ export default function Nav() {
         >
           <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <NavItem href="/" name="Home" />
+            <NavItem href="/invite/check" name="Invite Lookup" />
           </ul>
         </div>
       </div>
