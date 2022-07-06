eee73c0733b21a51cff5dd6114a55227c8dac12c
Jim <compositr@compositr.dev>
2022-07-06 21:18:18 +1000

    feat: images
    Signed-off-by: Jim <compositr@compositr.dev>

--- components/partnered/PartnerCard.tsx
+++ components/partnered/PartnerCard.tsx
@@ -1,4 +1,5 @@
 import { PartneredServer } from "@prisma/client";
+import Image from "next/image";
 
 export default function PartnerCard({
   name,
@@ -7,28 +8,33 @@ export default function PartnerCard({
   description,
   createdAt,
   updatedAt,
+  image,
 }: PartneredServer) {
   return (
     <div className="card w-96 h-96 bg-base-100 shadow-xl border flex-1">
+      {image ? (
+        <figure>
+          <Image src={image} alt={`Server banner for ${name}`} layout="intrinsic" width={240} height={135}/>
+        </figure>
+      ) : null}
+
       <div className="card-body">
         <h2 className="card-title leading-tight text-lg">{name}</h2>
         <p className="leading-tight text-sm">{description}</p>
-        <div className="flex flex-row flex-wrap gap-2 justify-evenly">
-          <span className="badge badge-info ">Safe Server</span>
-          {partnered ? (
-            <span className="badge badge-success ">Partnered</span>
-          ) : null}
-        </div>
-      </div>
-      <div className="card-actions justify-center mb-2">
         <a
           href={invite}
-          className="btn btn-wide btn-success"
-          target={"_blank"}
           rel="noreferrer"
+          target={"_blank"}
+          className="link link-primary text-center"
         >
-          Join
+          Click to join
         </a>
+        <div className="card-actions justify-end">
+          <span className="badge badge-info">Safe Server</span>
+          {partnered ? (
+            <span className="badge badge-success">Partnered</span>
+          ) : null}
+        </div>
       </div>
     </div>
   );
